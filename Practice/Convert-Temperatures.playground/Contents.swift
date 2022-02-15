9b80ba70ccbbf3577de2aee6866782ba628cc925
LeehanChoi <devprworld@gmail.com>
2022-02-16 00:36:02 +0900

    Update Convert-Temperatures.playground
    Checkpoint 1 Solution

--- Practice/Convert-Temperatures.playground/Contents.swift
+++ Practice/Convert-Temperatures.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
+ /*
+  1. Creates a constant holding any temperature in Celsius.
+  2. Converts it to Fahrenheit by multiplying by 9, dividing by 5, then adding 32.
+  3. Prints the result for the user, showing both the Celsius and Fahrenheit values.
+  */
 
-var greeting = "Hello, playground"
+let Celsius = 22
+let Fahrenheit = Celsius * 9/5 + 32
+let announce = "Today's temperature is \(Celsius)°C and \(Fahrenheit)°F."
+print(announce)
+
+// option + shift + 8 => get the degrees symbol. 
